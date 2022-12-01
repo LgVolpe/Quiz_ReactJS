303ea6fecc916045bb73fc4fd2c9b133b7c38266
Luis Guilherme Volpe <104277176+LgVolpe@users.noreply.github.com>
2022-12-01 20:58:36 -0300

    Final Project

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,23 +1,23 @@
-import { useContext } from 'react';
-import { QuizContext } from '../context/quiz';
-
-import Quiz from '../img/quiz.svg';
+import { useContext } from "react";
+import { QuizContext } from "../context/quiz";
 
 import "./Welcome.css";
 
-const Welcome = () => {
-  const quizState = useContext(QuizContext);
+import Quiz from "../img/quiz.svg";
 
-  console.log(quizState);
+const Welcome = () => {
+  const [quizState, dispatch] = useContext(QuizContext);
 
   return (
     <div id="welcome">
-        <h2>Seja bem-vindo</h2>
-        <p>Clique no botão abaixo para começar:</p>
-        <button>Iniciar</button>
-        <img src={Quiz} alt="Início do Quiz" />
+      <h2>Seja bem-vindo</h2>
+      <p>Clique no botão abaixo para começar:</p>
+      <button onClick={() => dispatch({ type: "CHANGE_STAGE" })}>
+        Iniciar
+      </button>
+      <img src={Quiz} alt="Início do Quiz" />
     </div>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
